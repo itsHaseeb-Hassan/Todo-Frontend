f2f0f0afca830c47a18780b0d54a89e9cb6c2137
haseeb hassan <its.haseebhassan07@gmail.com>
2024-07-26 08:35:52 +0500

    mobile screen view

--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -30,7 +30,7 @@ const LoginScreen = () => {
   return (
     <>
       <h1 className="text-center p-9 text-3xl uppercase">Login User</h1>
-      <div className="mx-auto w-[30%] h-[50%] p-4 bg-gray-300 rounded-md backdrop-filter backdrop-blur-md bg-opacity-20">
+      <div className="mx-auto w-[50%] h-[50%] p-4 bg-gray-300 rounded-md backdrop-filter backdrop-blur-md bg-opacity-20">
         <form onSubmit={handleSubmit}>
           <FormInput text="Email" type="email" placeholder="Enter your Email" value={formdata.email} name="email" onChange={handleInput} />
           <FormInput text="Password" type="password" placeholder="Enter your Password" value={formdata.password} name="password" onChange={handleInput} />
