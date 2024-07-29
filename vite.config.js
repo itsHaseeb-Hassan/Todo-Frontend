1a3902859407f56c80e06c05326d9bf8dde42793
haseeb hassan <its.haseebhassan07@gmail.com>
2024-07-29 16:11:21 +0500

    some changes

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://todo-bckend.vercel.app',
+        target: 'https://backend-render-70i8.onrender.com',
         changeOrigin: true,
         secure: true,
         rewrite: (path) => path.replace(/^\/api/, '')
